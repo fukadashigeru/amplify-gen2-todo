39b4833e6f39e16ffc6cd06bb8a88d9b4f07aef0
Fukada Shigeru <sakusaku721@gmail.com>
2025-11-03 22:57:09 +0900

    schema

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,3 +1,42 @@
+// // amplify/data/resource.ts
+// import { a, defineData, type ClientSchema } from '@aws-amplify/backend';
+
+// const schema = a.schema({
+//   Todo: a.model({
+//     content: a.string(),
+//     isDone: a.boolean(),
+//   }).authorization(allow => [ allow.publicApiKey() ]),
+// });
+
+// export type Schema = ClientSchema<typeof schema>;
+
+// export const data = defineData({
+//   schema,
+//   authorizationModes: {
+//     defaultAuthorizationMode: 'apiKey',
+//     apiKeyAuthorizationMode: { expiresInDays: 30 },
+//   },
+// });
+
+// // amplify/data/resource.ts
+// import { a, defineData, type ClientSchema } from '@aws-amplify/backend';
+
+// const schema = a.schema({
+//   Todo: a.model({
+//     content: a.string(),
+//     isDone: a.boolean(),
+//   }).authorization(allow => [ allow.owner() ]),  // ← ここをownerに
+// });
+
+// export type Schema = ClientSchema<typeof schema>;
+
+// export const data = defineData({
+//   schema,
+//   authorizationModes: {
+//     defaultAuthorizationMode: 'userPool',   // ← ここも userPool に
+//   },
+// });
+
 // amplify/data/resource.ts
 import { a, defineData, type ClientSchema } from '@aws-amplify/backend';
 
@@ -5,6 +44,11 @@ const schema = a.schema({
   Todo: a.model({
     content: a.string(),
     isDone: a.boolean(),
+  }).authorization(allow => [ allow.owner() ]),
+
+  PublicPost: a.model({
+    title: a.string(),
+    body: a.string(),
   }).authorization(allow => [ allow.publicApiKey() ]),
 });
 
@@ -13,7 +57,7 @@ export type Schema = ClientSchema<typeof schema>;
 export const data = defineData({
   schema,
   authorizationModes: {
-    defaultAuthorizationMode: 'apiKey',
-    apiKeyAuthorizationMode: { expiresInDays: 30 },
+    defaultAuthorizationMode: 'userPool', // デフォルト: ログインユーザー、ログイン済みはJWTで認証
+    apiKeyAuthorizationMode: { expiresInDays: 30 }, // 公開用も追加、匿名でもOK
   },
 });
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import ReactDOM from 'react-dom/client'
 import { Amplify } from 'aws-amplify'
 import outputs from '../amplify_outputs.json'
 import Root from './Root'  // ← ここをAppからRootに変更
+// import App from './App'
 
 Amplify.configure(outputs)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Root />
+    {/* <App /> */}
   </React.StrictMode>,
 )
